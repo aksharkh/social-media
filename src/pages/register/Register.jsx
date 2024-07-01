c937de47ce0803fb65794dfb446bd9d9c86fb0b4
aksharkh <aksharkh04@gmail.com>
2024-07-01 17:04:41 +0530

    changed the social media name

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -6,12 +6,7 @@ const Register = () => {
     <div className="register">
       <div className="card">
         <div className="left">
-          <h1>Lama Social.</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
-            alias totam numquam ipsa exercitationem dignissimos, error nam,
-            consequatur.
-          </p>
+          <h1>Social Media</h1>
           <span>Do you have an account?</span>
           <Link to="/login">
           <button>Login</button>
