7e92353f58a2caf9c6e4f49bc205074d5e41810e
aksharkh <aksharkh04@gmail.com>
2024-07-06 22:54:17 +0530

    added dark theme in app.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
   const currentUser = true;
   const Layout = () => {
     return (
-      <div className="theme-light">
+      <div className="theme-dark">
         <Navbar />
         <div style={{display: "flex"}}>
           <Leftbar />
